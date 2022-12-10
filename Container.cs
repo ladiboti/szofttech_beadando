a0282196103a3d1d2df5847bb90c326de6b8cea3
Adrián <adreszka2001@gmail.com>
2022-12-10 22:54:31 +0100

    event, container class

---
+++ Container.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szofttech
+{
+    internal class Container
+    {
+        private List<Event> events;
+        private List<Student> students;
+    }
+}
---
+++ Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szofttech
+{
+    internal class Event
+    {
+        private Senior organizer;
+        private String descripiton;
+        private Date date;
+        private String place;
+
+        public getOrganizer();
+        public getDescription();
+        public getDate();
+        public getPlace();
+    }
+}
