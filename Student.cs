b505df36e4fde7b6617951dab3aa99742a553491
ladiboti <ladiboti@gmail.com>
2022-12-10 22:59:13 +0100

    student, notification class added

---
+++ Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szofttech
+{
+  internal class Student
+  {
+    private List<Notification> notificationList;
+    private List<string>       bicycles;
+
+    private string name;
+  }
+}
