c44dc8511aeeaed97923ce6cc03dc1cd6f27b719
DallosK <dallos.kristof@gmail.com>
2022-12-10 23:22:39 +0100

    p

--- Administrator.cs
+++ Administrator.cs
@@ -12,42 +12,42 @@ namespace szofttech
 
         private void addNewStudent()
         {
-
+            throw new NotImplementedException();
         }
 
         private void promoteStudent()
         {
-
+            throw new NotImplementedException();
         }
 
         public void addRequest()
         {
-
+            throw new NotImplementedException();
         }
 
         private void approveRequest()
         {
-
+            throw new NotImplementedException();
         }
 
         private void addNotification()
         {
-
+            throw new NotImplementedException();
         }
 
         private void addObligation()
         {
-
+            throw new NotImplementedException();
         }
 
         private void modifyDisciplinaryState()
         {
-
+            throw new NotImplementedException();
         }
 
         private void moveToRoom()
         {
-
+            throw new NotImplementedException();
         }
   }
 }
