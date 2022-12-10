21ff7e966f39f1beae3231526296309e93362252
DallosK <dallos.kristof@gmail.com>
2022-12-10 23:19:05 +0100

    ff

--- Request.cs
+++ Request.cs
@@ -8,5 +8,16 @@ namespace szofttech
 {
     internal class Request
     {
+        private Student sender { get; }
+        private string message { get; }
+        private int status { get; set; }
+
+        public Request(Student sender, string message)
+        {
+            this.sender = sender;
+            this.message = message;
+            this.status = 0;
+        }
+
     }
 }
