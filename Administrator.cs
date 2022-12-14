86eead9edab85250282c366cbda1ac51ee95a410
DallosK <dallos.kristof@gmail.com>
2022-12-14 15:34:56 +0100

    Admin

--- Administrator.cs
+++ Administrator.cs
@@ -18,8 +18,6 @@ namespace szofttech
             string neptunCode;
             int roomNumber = -1;
             string major;
-
-            //KÉRD BE ADRIÁN!!!!
             string password = null;
             Console.WriteLine("Please give us the Student name!");
             name = Console.ReadLine();
@@ -65,31 +63,30 @@ namespace szofttech
 
         private void promoteStudentToSenior()
         {
-            string neptunCode;
-
-            Console.WriteLine("Please give us the neptun code of the Student you want to promote!");
-            neptunCode = Console.ReadLine();
-            
-            //ez a feltétel így biztosan nem lesz jó
-            while (neptunCode == "") {
-
-                Console.WriteLine("The given neptun code is invalid! Please give us a valid neptun code!");
-                neptunCode = Console.ReadLine();
+            Console.Write("Give the neptun code of the student who you wish to promote to senior rank: ");
+            string tempNeptun = Console.ReadLine().ToUpper();
+            while (tempNeptun == "")
+            {
+                Console.WriteLine("No neptun code given. Give one!");
+                tempNeptun = Console.ReadLine();
             }
-
-            //rossz neptun kódnál hibát fog dobni
-            Student student = new Student();
-            student = Container.students.Find(s => s.neptunCode == neptunCode);
+            if (Container.students.Exists(x => x.neptunCode == tempNeptun) == true)
+            {
+                Student student = new Student();
+                student = Container.students.Find(x => x.neptunCode == tempNeptun);
+                Container.students.Add(new Senior(student.notificationList, student.bicycles, student.name,
+                                              student.neptunCode, student.major, student.password, student.roomNumber));
+                Container.students.Remove(student);
+                Console.WriteLine("The selected student is sucessfully promoted!");
+            }
+            else
+                Console.WriteLine("The person does not presented in the list");
 
             //TODO: frissiteni kell majd a jsont!!!!
             //public Senior(List<Notification> notificationList, List<string> bicycles, string name, string neptunCode, string major, int roomNumber)
             //      : base(notificationList, bicycles, name, neptunCode, major, roomNumber)
-            Container.students.Add(new Senior(student.notificationList, student.bicycles, student.name,
-                                              student.neptunCode, student.major, student.password, student.roomNumber));
 
-            Container.students.Remove(student);
-             // folytatás később
-    }
+        }
 
         public static void addRequest(Request request)
         {
@@ -196,5 +193,5 @@ namespace szofttech
                 new Notification(addNotification(), new Date(DateTime.Now.Year, DateTime.Now.Month,
                                                              DateTime.Now.Day, DateTime.Now.Hour, DateTime.Now.Minute)));
         }
-  }
+    }
 }
